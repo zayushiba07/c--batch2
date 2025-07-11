32ef28742d766f142b4892c37d2ff729b92b1860
zayushiba07 <zayushiba07@gmail.com>
2025-07-11 19:30:50 +0530

    operation

---
+++ 1_arithemetic_op.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    /*cout<<"Enter the number :";
+    cin>>a>>b;*/
+    cout<<"Enter number 1:";
+    cin>>a;
+    cout<<"Enter number 2:";
+    cin>>b;
+    cout<<"Add :"<<a+b<<endl;
+    cout<<"sub :"<<a-b<<endl;
+    cout<<"mul :"<<a*b<<endl;
+    cout<<"Div :"<<a/b<<endl;
+    cout<<"mod :"<<a%b<<endl;
+    cout<<"Inc :"<<a++<<endl;
+    cout<<"Dec :"<<b--<<endl;
+    return 0;
+}

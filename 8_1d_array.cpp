5c0c541b69011e2dc94beeb6dfee3577200c7c8e
zayushiba07 <zayushiba07@gmail.com>
2025-07-17 20:12:11 +0530

    array

---
+++ 8_1d_array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int size = 5;
+
+    int eid[size] = {101, 102, 103, 104, 105};
+    string ename[size] = {"poonam", "ankit", "rajesh", "nikita", "manish"};
+
+    cout << "eid \t ename " << endl;
+
+    for(int i = 0; i <=size; i++)  // Correct loop range
+    {
+        cout << eid[i] << "\t" << ename[i] << endl;
+    }
+
+    return 0;
+}
